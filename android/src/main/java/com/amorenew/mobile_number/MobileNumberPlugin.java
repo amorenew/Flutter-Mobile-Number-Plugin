af4660a05c7736be9fb35e2f6880f379425cdcd1
Amr Abdelwahab <amorenew@gmail.com>
2024-12-29 23:43:01 +0200

    upgrade gradle

--- android/src/main/java/com/amorenew/mobile_number/MobileNumberPlugin.java
+++ android/src/main/java/com/amorenew/mobile_number/MobileNumberPlugin.java
@@ -12,7 +12,6 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -30,219 +29,196 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener;
 
-/**
- * MobileNumberPlugin
- */
-public class MobileNumberPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, RequestPermissionsResultListener {
-    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
-    final String Event_phonePermissionResult = "requestPhonePermission=";
-    private EventChannel.EventSink permissionEvent;
+public class MobileNumberPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
+    private static final int REQUEST_PHONE_PERMISSION = 123;
+    private static final String TAG = "MobileNumberPlugin";
+
     private Context applicationContext;
     private Activity activity;
     private TelephonyManager telephonyManager;
-    private Result result;
+    private Result pendingResult;
     private MethodChannel methodChannel;
     private EventChannel permissionEventChannel;
-
-
-    /**
-     * Plugin registration.
-     */
-    public static void registerWith(Registrar registrar) {
-        final MobileNumberPlugin instance = new MobileNumberPlugin();
-        instance.onAttachedToEngine(registrar.context(), registrar.messenger(), registrar.activity());
-    }
+    private EventChannel.EventSink permissionEvent;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger(), null);
+        onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
     }
 
-    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger, Activity _activity) {
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
         this.applicationContext = applicationContext;
-        if(_activity!=null)
-            this.activity=_activity;
+
         methodChannel = new MethodChannel(messenger, "mobile_number");
         methodChannel.setMethodCallHandler(this);
+
         permissionEventChannel = new EventChannel(messenger, "phone_permission_event");
         permissionEventChannel.setStreamHandler(new EventChannel.StreamHandler() {
             @Override
-            public void onListen(Object o, EventChannel.EventSink eventSink) {
+            public void onListen(Object args, EventChannel.EventSink eventSink) {
                 permissionEvent = eventSink;
             }
 
             @Override
-            public void onCancel(Object o) {
-
+            public void onCancel(Object args) {
+                permissionEvent = null;
             }
         });
     }
 
     @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        this.pendingResult = result;
+        telephonyManager = (TelephonyManager) applicationContext.getSystemService(Context.TELEPHONY_SERVICE);
 
-    }
-
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        //MobileNumberPlugin.activity = activityPluginBinding.getActivity();
-        //activityV2 = activityPluginBinding.getActivity();
-        activity = activityPluginBinding.getActivity();
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
-
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-
-    }
-
-    @Override
-    public void onMethodCall(MethodCall call, Result result) {
-        this.result = result;
-        final String method_GetMobileNumber = "getMobileNumber";
-        final String method_hasPhonePermission = "hasPhonePermission";
-        final String method_requestPhonePermission = "requestPhonePermission";
         switch (call.method) {
-            case method_GetMobileNumber:
-                telephonyManager = (TelephonyManager) applicationContext
-                        .getSystemService(Context.TELEPHONY_SERVICE);
-                getMobileNumber();
+            case "getMobileNumber":
+                handleGetMobileNumber();
                 break;
-            case method_hasPhonePermission:
+            case "hasPhonePermission":
                 result.success(hasPhonePermission());
                 break;
-            case method_requestPhonePermission:
+            case "requestPhonePermission":
                 requestPhonePermission();
                 break;
             default:
                 result.notImplemented();
-                break;
         }
+    }
+
+    private String getRequiredPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Manifest.permission.READ_PHONE_NUMBERS;
+        }
+        return Manifest.permission.READ_PHONE_STATE;
     }
 
     private boolean hasPhonePermission() {
-        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
-            return ContextCompat.checkSelfPermission(applicationContext,
-                    Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_GRANTED;
-        } else {
-            return ContextCompat.checkSelfPermission(applicationContext,
-                    Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
-        }
+        return ContextCompat.checkSelfPermission(applicationContext, getRequiredPermission())
+                == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestPhonePermission() {
-        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.READ_PHONE_NUMBERS)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.READ_PHONE_NUMBERS}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+        if (activity == null) {
+            if (permissionEvent != null) {
+                permissionEvent.error("ACTIVITY_NULL", "Activity is null", null);
             }
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.READ_PHONE_STATE)) {
-            } else {
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
-            }
+            return;
         }
+
+        ActivityCompat.requestPermissions(
+                activity,
+                new String[]{getRequiredPermission()},
+                REQUEST_PHONE_PERMISSION
+        );
     }
 
-    private void getMobileNumber() {
+    private void handleGetMobileNumber() {
         if (!hasPhonePermission()) {
             requestPhonePermission();
-        } else {
-            // Permission has already been granted
-            generateMobileNumber();
-
+            return;
         }
-    }
 
-    @SuppressLint("HardwareIds")
-    private void generateMobileNumber() {
-        JSONArray simJsonArray = new JSONArray();
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
-            for (SubscriptionInfo subscriptionInfo : getSubscriptions()) {
-                SimCard simCard = new SimCard(telephonyManager, subscriptionInfo);
-                simJsonArray.put(simCard.toJSON());
+        try {
+            List<SimCard> simCards = getSimCards();
+            if (simCards != null && !simCards.isEmpty()) {
+                JSONArray simJsonArray = new JSONArray();
+
+                for (int i = 0; i < simCards.size(); i++) {
+                    simJsonArray.put(simCards.get(i).toJSON());
+                }
+                pendingResult.success(simJsonArray.toString());
+            } else {
+                pendingResult.error(
+                        "UNAVAILABLE",
+                        "Phone number is not available",
+                        null
+                );
             }
+        } catch (Exception e) {
+            pendingResult.error(
+                    "ERROR",
+                    "Error retrieving phone number: " + e.getMessage(),
+                    null
+            );
         }
-        if (simJsonArray.length()==0) {
-            SimCard simCard = getSingleSimCard();
-            if (simCard != null) {
-                simJsonArray.put(simCard.toJSON());
+    }
+
+    @SuppressLint({"HardwareIds", "MissingPermission"})
+    private List<SimCard> getSimCards() {
+        List<SimCard> simCards = new ArrayList<>();
+
+        if (!hasPhonePermission()) {
+            return simCards;
+        }
+
+        String phoneNumber = null;
+
+        try {
+            // Try to get phone number from default SIM
+            phoneNumber = telephonyManager.getLine1Number();
+            if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                simCards.add(new SimCard(telephonyManager));
             }
-        }
+            // If default method failed, try to get from SubscriptionManager (Android 5.1 and above)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                SubscriptionManager subscriptionManager = (SubscriptionManager)
+                        applicationContext.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
 
-        if (simJsonArray.toString().isEmpty()) {
-            Log.d("UNAVAILABLE", "No phone number on sim card#3");
-            result.error("UNAVAILABLE", "No phone number on sim card", null);
-        } else result.success(simJsonArray.toString());
-    }
+                if (subscriptionManager != null) {
+                    List<SubscriptionInfo> subscriptionInfos = subscriptionManager.getActiveSubscriptionInfoList();
+                    if (subscriptionInfos != null && !subscriptionInfos.isEmpty()) {
+                        // Get the first active subscription
+                        SubscriptionInfo subscriptionInfo = subscriptionInfos.get(0);
 
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            // For Android N (7.0) and above
+                            TelephonyManager subscriptionTelephonyManager =
+                                    telephonyManager.createForSubscriptionId(subscriptionInfo.getSubscriptionId());
+                            String subscriptionPhoneNumber = subscriptionTelephonyManager.getLine1Number();
+                            if (subscriptionPhoneNumber != null && !subscriptionPhoneNumber.isEmpty() && !subscriptionPhoneNumber.equals(phoneNumber)) {
+                                simCards.add(new SimCard(subscriptionTelephonyManager));
+                            }
 
-    @SuppressLint("HardwareIds")
-    SimCard getSingleSimCard() {
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_DENIED
-                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED) {
-            Log.e("UNAVAILABLE", "No phone number on sim card Permission Denied#2", null);
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error getting phone number: " + e.getMessage());
             return null;
-        } else if (telephonyManager.getLine1Number() == null || telephonyManager.getLine1Number().isEmpty()) {
-            Log.e("UNAVAILABLE", "No phone number on sim card#2", null);
-            return null;
         }
-        return new SimCard(telephonyManager);
+
+        return simCards;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
-    List<SubscriptionInfo> getSubscriptions() {
-        final SubscriptionManager subscriptionManager = (SubscriptionManager) activity.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_DENIED
-                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED) {
-            Log.e("UNAVAILABLE", "No phone number on sim card Permission Denied#1", null);
-            return new ArrayList<>();
-        } else if (subscriptionManager == null) {
-            Log.e("UNAVAILABLE", "No phone number on sim card#1", null);
-            return new ArrayList<>();
-        }
-        return subscriptionManager.getActiveSubscriptionInfoList();
+    // ActivityAware implementation
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
     }
-
 
     @Override
-    public boolean onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                              @NonNull int[] grantResults) {
-        // If request is cancelled, the result arrays are empty.
-        if (requestCode == MY_PERMISSIONS_REQUEST_READ_PHONE_STATE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (permissionEvent != null)
-                    permissionEvent.success(true);
-                generateMobileNumber();
-                return true;
-            } else {
-                if (permissionEvent != null)
-                    permissionEvent.success(false);
-            }
-        }
-        result.error("PERMISSION", "onRequestPermissionsResult is not granted", null);
-        return false;
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
     }
 
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
 
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+
+    // FlutterPlugin implementation
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        permissionEventChannel.setStreamHandler(null);
+    }
 }
